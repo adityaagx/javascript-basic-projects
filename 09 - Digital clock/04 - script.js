8cc1ac0b5e6a9c0c751539bf4c06efb6c93fbf9c
aditya <adityaagarwal382@gmail.com>
2026-02-16 20:24:03 +0530

    Add js code for Digital Clock task

---
+++ 09 - Digital clock/04 - script.js
@@ -0,0 +1,18 @@
+let clock = document.querySelector('#clock');
+
+function updateClock(){
+    let now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    if(hours < 10) hours = "0" + hours;
+    if(minutes < 10) minutes = "0" + minutes;
+    if(seconds < 10) seconds = "0" + seconds;
+
+    clock.innerHTML = `${hours} : ${minutes} : ${seconds}`
+};
+
+setInterval(updateClock, 1000);
+updateClock();
