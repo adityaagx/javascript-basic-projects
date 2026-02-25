341ed450f7ecf059bb1ff899b38d700febb00f50
aditya <adityaagarwal382@gmail.com>
2026-02-25 19:08:40 +0530

    Changed notes app html code

---
+++ notes-app/script.js
@@ -0,0 +1,9 @@
+let addNote = document.querySelector('#addNote');
+let addBtn = document.querySelector('#addBtn');
+let notesList = document.querySelector('#notesList');
+let editBtn = document.querySelector('#edit');
+let deleteBtn = document.querySelector('#delete');
+
+addBtn.addEventListener("click", () = {
+    
+})
